cccd2dbf482005ee9feb6b2e3534b642a3fb0319
piper_of_the_dawn <kumarshan25@gmail.com>
2023-11-27 20:44:50 +0100

    first commit

---
+++ reportOutline.md
@@ -0,0 +1,26 @@
+# Dataframe Comparison Report
+
+## Comparison Metadata
+{metadata}
+{overallResult}
+
+
+## Count Comparison
+{countComparison}
+
+## Schema Comparison
+{schemaComparison}
+
+## Test 1: Comparison of the String Columns
+Jaccard Similarity is defined as the ratio of the cardinality of the intersection of the two sets (expected and tested) divided by the cardinality of the union of the sets. 
+
+J(A,B) = n(A ∩ B) / n(A ∪ B).
+
+The Jaccard Similarity is a number between 0 and 1. 0 means the two sets are completely different, 1 means the two sets are identical. Jaccard Similarity is an order insensitive measure of set similarity. Jaccard Similarity of 1 does not imply the order of values in the two columns are identical.
+
+## Test 2: Comparison of the Numeric Columns
+
+
+
+
+
---
+++ test_compare.py
@@ -0,0 +1,20 @@
+import polars as pl
+from compare_datasets import Compare
+import pandas as pd
+def test_compare():
+    data = pl.scan_csv('compare_datasets/tests/mfPrices.csv', try_parse_dates=True).fetch(100)
+    data_changed = pl.scan_csv('compare_datasets/tests/mfPrices_changed.csv',try_parse_dates=True).fetch(100)
+    # print(data.schema)
+    c = Compare(tested=data_changed,expected=data, verbose=False)
+    assert c is not None 
+    print(c)
+
+def test_compare_pandas():
+    data = pd.read_csv('compare_datasets/tests/mfPrices.csv', parse_dates=True, nrows=100)
+    data_changed = pd.read_csv('compare_datasets/tests/mfPrices_changed.csv', nrows=100)
+    # print(data.schema)
+    c = Compare(tested=data_changed,expected=data, verbose=False)
+    assert c is not None 
+    print(c)
+    
+test_compare()
